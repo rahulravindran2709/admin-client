8de82616f06d39c64e3f52d1460ca3ec148b49af
Hari Gopal <mail@harigopal.in>
2017-01-22 15:18:58 +0530

    Extract Routes

--- components/index.jsx
+++ components/index.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router, Route, browserHistory} from 'react-router'
-import Dashboard from 'Dashboard'
-import Login from 'Login'
+import Routes from 'Routes'
 
 ReactDOM.render(
-  (
-    <Router history={browserHistory}>
-      <Route path='/' component={Login} />
-      <Route path='dashboard' component={Dashboard} />
-    </Router>
-  ), document.getElementById('root')
+  <Routes/>, document.getElementById('root')
 )
