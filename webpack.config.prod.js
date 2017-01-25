29a97517543139c8be8d618d4f34e0d08ff5c829
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-01-26 00:44:05 +0530

    Prod version of webpack first draft

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,37 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackBaseConfig =require('./webpack.config.base');
+const APP_ENTRY= path.resolve(__dirname, 'src/components','index.js');
 
-
-module.exports = config
+module.exports = webpackBaseConfig({
+  devtool: 'source-map',
+  entry: [
+    APP_ENTRY
+  ],
+  output: {
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+         minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    })
+  ]
+})
